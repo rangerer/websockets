20267c7f7dc229ff679cf0b65db40d45faf43386
Roland Angerer <roland.angerer@beenergised.com>
2018-02-26 09:59:43 +0100

    add echo server from WebSockets documentation

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ see [WebSockets Library](http://websockets.readthedocs.io/en/stable/index.html)
 ## Usage
 
     mkvirtualenv -p /usr/bin/python3 -r requirements.txt websockets
+
+Start the echo server with
+
+    ./server.py
---
+++ server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(
+        websockets.serve(echo, 'localhost', 8765))
+    asyncio.get_event_loop().run_forever()
