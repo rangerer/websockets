a75ccda96f04375a1476e5c45f4beedd95f4e07a
Roland Angerer <roland.angerer@beenergised.com>
2018-02-26 12:31:50 +0100

    allow input of message and show output

--- client.py
+++ client.py
@@ -5,7 +5,10 @@ import websockets
 
 async def client(uri):
     async with websockets.connect(uri) as websocket:
-        await websocket.send("Hello world!")
+        message = input("> ")
+        await websocket.send(message)
+        response = await websocket.recv()
+        print("< {}".format(response))
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(
